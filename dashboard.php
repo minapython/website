80174bbb60338473cb7a7507b36a37c8934008ec
root <root@ip-172-31-2-209.us-east-2.compute.internal>
2021-03-17 19:23:20 +0000

    dashboard modified and dropdown menu added

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,49 @@ include_once 'source/session.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <style>
+.dropbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #ddd;}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {display: block;}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
+Try it Yourself »
+</style>
 <script>
   alert('Successfully logged in. Welcome to the dashboard.');
 </script>
@@ -23,8 +66,15 @@ include_once 'source/session.php';
     <?php endif ?>
 
     <?php echo "<h1> Welcome ".$_SESSION['username']." To Dashboard.<br>
-    You will be redirected shortly. </h1>" ?>
+    Enjoy your stay.</h1>" ?>
     <h2><a href="logout.php">Logout</a></h2>
-
+    <div class="dropdown">
+  <button class="dropbtn">More Pages</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
 </body>
 </html>
